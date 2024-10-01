74371a9993f3e6affb707d450df8800b450c3a50
Ilinca Hirtopanu <ilinca.b.hirtopanu@gmail.com>
2024-10-01 02:33:31 -0400

    loads of functionality

--- index.js
+++ index.js
@@ -206,6 +206,7 @@ app.get('/api/people', async (req, res) => {
 //     }
 // });
 
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
